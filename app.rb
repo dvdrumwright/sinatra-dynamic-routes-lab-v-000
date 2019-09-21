34804c29479e817a7c697e2decea26bc6f1648c9
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-09-21 19:20:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,12 +4,12 @@ class App < Sinatra::Base
   # Write your code here!
 
   get "/reversename/:name" do
-    @name = params[:name].reverse
+    name = params[:name].reverse
   end
 
   get '/square/:number' do
-    @number = params[:num]
-    (:numb.to_i**2).to_s
+    numb = params[:number]
+    (numb.to_i**2).to_s
   end
 
 
