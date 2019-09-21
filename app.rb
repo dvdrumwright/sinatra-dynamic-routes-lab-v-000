8302246dc86becd2fa5b12fd61e60000eda79f19
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-09-21 20:22:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,47 +19,47 @@ class App < Sinatra::Base
      end
      PHRASE
   end
-
-  get '/say/:word1/:word2/:word3/:word4/:word5' do
-   "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
-  end
-
-  get '/:operation/:number1/:number2' do
-       operation = params[:operation]
-       number1 = params[:number1].to_i
-       number2 = params[:number2].to_i
-       if operation == "add"
-          a = number1+number2
-      elsif operaton == "subtract"
-          a = number1-number2
-      elsif operation == "multiply"
-          a = number1*number2
-     elsif operation == "divide"
-          a = number1/number2
-     else
-          a = "Your answer is invalid ! "
-     end
-          a.to_s
-   end
-
-
-  #  get '/:operation/:number1/:number2' do
+  # 
+  # get '/say/:word1/:word2/:word3/:word4/:word5' do
+  #  "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  # end
+  #
+  # get '/:operation/:number1/:number2' do
   #      operation = params[:operation]
   #      number1 = params[:number1].to_i
   #      number2 = params[:number2].to_i
   #      if operation == "add"
-  #        answer = number1 + number2
-  #      elsif operation == "subtract"
-  #        answer = number1 - number2
-  #      elsif operation == "multiply"
-  #        answer = number1 * number2
-  #      elsif operation == "divide"
-  #        answer = number1 / number2
-  #      else
-  #        answer = "Unable to perform this operation"
-  #      end
-  #      answer.to_s
+  #         a = number1+number2
+  #     elsif operaton == "subtract"
+  #         a = number1-number2
+  #     elsif operation == "multiply"
+  #         a = number1*number2
+  #    elsif operation == "divide"
+  #         a = number1/number2
+  #    else
+  #         a = "Your answer is invalid ! "
   #    end
+  #         a.to_s
+  #  end
+
+
+   get '/:operation/:number1/:number2' do
+       operation = params[:operation]
+       number1 = params[:number1].to_i
+       number2 = params[:number2].to_i
+       if operation == "add"
+         answer = number1 + number2
+       elsif operation == "subtract"
+         answer = number1 - number2
+       elsif operation == "multiply"
+         answer = number1 * number2
+       elsif operation == "divide"
+         answer = number1 / number2
+       else
+         answer = "Unable to perform this operation"
+       end
+       answer.to_s
+     end
 
 
 
